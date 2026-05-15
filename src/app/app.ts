2190ea81f2b89636a3b39da13b5eb9aac718f78c
Coco10023 <mauricio2003@live.se>
2026-05-15 20:16:20 +0200

    Hämtar datan i app.ts för att kunna använda servicen för att läsa in kurserna

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,44 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { CourseService } from './services/course.service';
+import { Course } from './models/course';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CommonModule, FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  protected readonly title = signal('labb4-angular');
+export class App implements OnInit {
+
+  courses: Course[] = [];  // Här sparas alla kurser
+  filteredCourses: Course[] = [];  // Här sparas filtrerade kurser
+
+  searchText: string = '';
+
+  constructor(private courseService: CourseService) {}
+
+  ngOnInit(): void { // Hämtar JSON-datan från min service 
+    this.courseService.getCourses().subscribe((data) => {
+      this.courses = data;
+      this.filteredCourses = data;
+    });
+  }
+
+  filterCourses(): void { // Filtrerar sökningen 
+    const search = this.searchText.toLowerCase();
+
+    this.filteredCourses = this.courses.filter(course =>
+      course.code.toLowerCase().includes(search) ||
+      course.coursename.toLowerCase().includes(search)
+    );
+  }
+
+  sortCourses(field: keyof Course): void { // Sorterar tabellen 
+    this.filteredCourses.sort((a, b) =>
+      a[field].localeCompare(b[field])
+    );
+  }
 }
